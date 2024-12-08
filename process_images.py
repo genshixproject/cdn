1629fc7d3782d78f60f74af25c3d19047e95ab68
MoxForever <moxforever10@gmail.com>
2024-12-08 23:52:00 +0600

    add config support

--- process_images.py
+++ process_images.py
@@ -1,43 +1,55 @@
+from dataclasses import dataclass
 import os
+import json
 from PIL import Image
 
-IMAGE_SIZES = {
-    "small": 128,
-    "middle": 512,
-    "big": None,
-}
 
-TARGET_EXTENSIONS = ["png", "jpg", "webp"]
+Path = str | os.PathLike[str]
+KNOWN_FILES_EXT = ["png", "webp"]
 
 
-def process_images(source_dir, target_dir):
-    for root, _, files in os.walk(source_dir):
-        for file in files:
-            if file.lower().endswith((".png", ".jpg", ".jpeg", ".webp")):
-                process_image(os.path.join(root, file), source_dir, target_dir)
+@dataclass
+class Config:
+    target_sizes: dict[str, int | None]
+    target_extensions: list[str]
 
 
-def process_image(image_path, source_dir, target_dir):
-    image = Image.open(image_path).convert("RGB")
-    base_path = os.path.relpath(image_path, source_dir)
-    base_name, _ = os.path.splitext(base_path)
-
-    for size_name, min_size in IMAGE_SIZES.items():
-        for ext in TARGET_EXTENSIONS:
-            target_path = os.path.join(target_dir, base_name, f"{size_name}.{ext}")
-            os.makedirs(os.path.dirname(target_path), exist_ok=True)
-
-            if min_size is None:  # "big" size: keep original dimensions
-                image.save(target_path, format=ext.upper())
-            else:
-                resized_image = resize_image(image, min_size)
-                resized_image.save(target_path, format=ext.upper())
+def load_config(config_path: Path) -> Config | None:
+    if not os.path.exists(config_path):
+        return None
+    with open(config_path, "r") as file:
+        return Config(**json.load(file))
 
 
-def resize_image(image, min_size):
-    ratio = min(image.width, image.height) / min_size
-    new_size = (int(image.width / ratio), int(image.height / ratio))
-    return image.resize(new_size, Image.Resampling.LANCZOS)
+def image_resize(img: Image.Image, resize_to: int | None):
+    if resize_to is None:
+        return img
+
+    r_coef = (min if resize_to > 0 else max)(img.size) / abs(resize_to)
+    return img.resize(map(int, (img.size[0] / r_coef, img.size[1] / r_coef)))
+
+
+def process_image(source_path: Path, target_dir: Path, config: Config):
+    for name, size in config.target_sizes.items():
+        img = image_resize(Image.open(source_path), size)
+        size_dir = os.path.join(target_dir, name)
+        os.makedirs(size_dir, exist_ok=True)
+        for ext in config.target_extensions:
+            filename = os.path.splitext(os.path.basename(source_path))[0] + f".{ext}"
+            img.save(os.path.join(size_dir, filename))
+
+
+def process_dir(current_dir: Path, target_dir: Path, config: Config | None):
+    config_current = load_config(os.path.join(current_dir, "config.json")) or config
+    if config_current is None:
+        raise ValueError("No configuration found.")
+
+    for d in os.listdir(current_dir):
+        file = os.path.join(current_dir, d)
+        if os.path.isdir(file):
+            process_dir(file, os.path.join(target_dir, d), config_current)
+        elif os.path.splitext(file)[1].strip(".") in KNOWN_FILES_EXT:
+            process_image(file, target_dir, config_current)
 
 
 if __name__ == "__main__":
@@ -50,5 +62,5 @@ if __name__ == "__main__":
     parser.add_argument("target_dir", help="Directory to store the processed images.")
 
     args = parser.parse_args()
-
-    process_images(args.source_dir, args.target_dir)
+    config = load_config(os.path.join(args.source_dir, "config.json"))
+    process_dir(args.source_dir, args.target_dir, config)
